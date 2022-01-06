9bffd0bc3e6a6c9d2e94499c1ed5b43d7626899f
Nicolas Lorenzon <Nicoolorenzon7@gmail.com>
2022-01-06 15:02:58 -0300

    Update plantilla.py

--- plantilla.py
+++ plantilla.py
@@ -37,25 +37,6 @@ with smart_run(sesion):
     sesion.set_smart_location_hashtags([""], radius=20, limit=10) # Generar Hashtags inteligentes por la ubicacion, radus = distancia a la redonde en millas
 
 
-# LOCALIZACIONES
-#  Gim Vertigo 1026882572/gimnasio-vertigo/
-#  Costanera 226724549/parque-urquiza-costanera-parana/
-#  Echague 289490085/atletico-echague-club-oficial/
-#  Casa Ditalia 801529605/gim-casa-d-italia/
-#  Atlethic gym 1013859680/athletic-gimnasio-deportivo/
-#  centro fitness 128608701022840/cf-628-centro-de-fitness/
-#  PRC 55252289/parana-rowing-club/
-#  CCP 295017276/club-ciclista-parana/
-#  Bonafide 214856181988338/bonafide-parana/
-#  Gym cep 798170816/gimnasio-cep/
-#  One Finess center 1002999060/one-fitness-center/
-#  Arenasports 887993377/arenasports-gimnasio/
-# Suplementos gym 1467257293573449/suplementos-gym/
-# Zeus gym 1030444545/zeus-gym/
-# Parana fitness 614558312316896/parana-fitness-place/
-# Play fit 371837166536506/play-fit-training-center/
-# Pro gym 338923740281904/pro-gym-parana/
-# gymcenter 606740392796799/megaforce-gymcenter/
-sss
+
 
 
